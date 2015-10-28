50a4fbc6aa1206e710ec79428f97fbb495c21b98
Katzenmann <hani.awwad@hhu.de>
2015-10-28 21:55:49 +0100

    Update fibonacci.cxx

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,17 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+
+  int a=0;
+  int b=1;
+  int c;
+  for(int i=1; i<9; i++){
+  c=a+b;
+  a=b;
+  b=c;
+    }
+  cout << c << endl;
+  return 0;
+}
