f6a17e6e03ce82dbaca23cb965ab499bb9300625
Katzenmann <hani.awwad@hhu.de>
2015-10-29 10:49:47 +0100

    Update fibonacci.cxx

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -6,12 +6,15 @@ int main(){
 
   int a=0;
   int b=1;
+  int N;
   int c;
-  for(int i=1; i<9; i++){
+cin>>N;
+  for(int i=1; i<N; i++){
   c=a+b;
   a=b;
   b=c;
     }
   cout << c << endl;
+  cout << "N" << endl;
   return 0;
 }
